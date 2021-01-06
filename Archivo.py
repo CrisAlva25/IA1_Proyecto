7fce5bc01d7f46dbfa2444681276754c4aac1dec
Cristian David Alvarado Lopez <crisalva5@gmail.com>
2021-01-06 02:48:20 -0600

    terminando genetico
    asasdgasg

--- Archivo.py
+++ Archivo.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import csv  
 import math
 
 """
@@ -24,8 +26,14 @@ def lista_distancias_municipios():
         elif distancia < min_distancia:
             min_distancia = distancia
 
-    #print(lista_retorno)
+    #print(lista_retorno["Acatenango"])
     return lista_retorno, max_distancia, min_distancia
+
+def conver(tipo):
+    if tipo == "Traslado" or tipo == "FEMENINO":
+        return 0.0
+    else:
+        return 1.0
 
 #calculo de distancia en base a latitud y longitud
 def distancia_usac(latitud, longitud):
@@ -38,17 +46,16 @@ def distancia_usac(latitud, longitud):
     dlat = latitud_usac - latitud
     dlon = longitud_usac - longitud
 
-    R = 5372.795477598
+    R = 6372.795477598
     a = (math.sin(radianes*dlat/2))**2 + math.cos(radianes*latitud)*math.cos(radianes*latitud_usac)*(math.sin(radianes*dlon/2))**2
     distancia =2*R*math.asin(math.sqrt(a))
     return distancia
 
 def lectura_dataset():
-
     #obtenemos datos
     datos = []
     distancias, max_dist, min_dist = lista_distancias_municipios()
-    dataset = pd.read_csv("datasets\Dataset.csv")
+    errores = []
 
     #maximos
     max_edad = 1
@@ -59,40 +66,80 @@ def lectura_dataset():
     min_año = 1000
 
     #recorremos
-    for row in dataset.itertuples():
-        try:
-            #calculamos la maxima y minima edad
-            if row[3] > max_edad:
-                max_edad = row[3]
-            elif row[3] < min_edad:
-                min_edad = row[3]
+    with open('datasets\Dataset.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                try:
+                    #calculamos la maxima y minima edad
+                    if int(row[2]) > max_edad:
+                        max_edad = int(row[2])
+                    elif int(row[2]) < min_edad:
+                        min_edad = int(row[2])
 
-            #calculamos la maxima y minima año
-            if row[8] > max_año:
-                max_año = row[8]
-            elif row[8] < min_año:
-                min_año = row[8]
-
-            datos.append([row[2],row[3],row[8],distancias[row[7]]])
-        except:
-            print("no existe su municipio en nuestros datos " + row[7])
+                    #calculamos la maxima y minima año
+                    if int(row[7]) > max_año:
+                        max_año = int(row[7])
+                    elif int(row[7]) < min_año:
+                        min_año = int(row[7])
+                    #print(row[1])
+                    datos.append([row[1],row[2],row[7],distancias[row[6]],conver(row[0])])
+                except:
+                    errores.append("no existe su municipio en nuestros datos " + row[6])
+                line_count += 1
+        print(f'Processed {line_count} lines.')
 
     #realizamos el proceso de escalado en todos los elementos posibles
     #print(min_edad, max_edad, min_año, max_año, min_dist, max_dist)
-    retorno_escalado = []
-    for ele in datos:
-        retorno_escalado.append([
-            ele[0], 
+    #for error in errores:
+    #    print(error)
+    res = np.array(datos)
+    np.random.shuffle(res)
+    slice_point = int(len(datos)*0.7)
+    test = res[slice_point:]
+    train = res[:slice_point]
+    
+    test_x = []
+    test_y = []
+    train_x = []
+    train_y = []
+
+    for ele in test:
+        test_x.append([
+            float(conver(ele[0])),
             escalar_var(ele[1], min_edad, max_edad),
             escalar_var(ele[2], min_año, max_año),
-            escalar_var(ele[3], min_dist, max_dist),
+            escalar_var(ele[3], min_dist, max_dist)
         ])
-    return retorno_escalado
+        test_y.append([float(ele[4])])
 
+    for ele in train:
+        train_x.append([
+            float(conver(ele[0])),
+            escalar_var(ele[1], min_edad, max_edad),
+            escalar_var(ele[2], min_año, max_año),
+            escalar_var(ele[3], min_dist, max_dist)
+        ])
+        train_y.append([float(ele[4])])
+
+    #print(len(test_x))
+    #print(test_y)
+    #print(len(train_x))
+    #print(len(train_y))
+
+    train_x_orig = np.array(train_x).T
+    train_y_orig = np.array(train_y).T
+    test_x_orig = np.array(test_x).T
+    test_y_orig = np.array(test_y).T
+    #print(train_x_orig[1])
+
+    return train_x_orig, train_y_orig, test_x_orig, test_y_orig
 
 def escalar_var(xi, mini, maximo):
-    return (xi - mini) / (maximo - mini)
+    return (float(xi) - mini) / (maximo - mini)
     
-
 #lista_distancias_municipios()
 #lectura_dataset()
--- main.py
+++ main.py
@@ -1,3 +1,120 @@
 import Archivo as Ar
+from Util import Plotter
+from Neural_Network.Data import Data
+from Neural_Network.Model import NN_Model
+import random
 
-print(Ar.lectura_dataset())
+ONLY_SHOW = False
+
+class Individuo:
+    def __init__(self, constantes,fitness):
+        self.constantes = constantes
+        self.fitness = fitness
+
+def entrenar_neuronas(train_set, val_set, capas1, ind):
+    alpha, lambd, iterations, keep_prob = get_hyperParams(ind[0], ind[1], ind[2], ind[3])
+    # Se define el modelo
+    nn1 = NN_Model(train_set, capas1, alpha, iterations, lambd, keep_prob)
+    #nn2 = NN_Model(train_set, capas1, alpha=0.01, iterations=50000, lambd=0.7, keep_prob=1)
+
+    # Se entrena el modelo
+    nn1.training(False)
+    #nn2.training(False)
+
+    # Se analiza el entrenamiento
+    #Plotter.show_Model([nn1])
+
+    #print('Entrenamiento Modelo 1')
+    #nn1.predict(train_set)
+    #print('Validacion Modelo 1')
+    return nn1.predict(val_set)
+
+#aqui empieza mi algoritmo genetico
+def crear_poblacion(train_set, val_set, capas1):
+    poblacion = []
+    for i in range(10):
+        temp = []
+        for j in range(4):
+            temp.append(random.randint(0,9))
+        nuevo = Individuo(temp, entrenar_neuronas(train_set, val_set, capas1, temp))
+        poblacion.append(nuevo)
+    return poblacion
+
+#Seleccion de padres
+def seleccionTorneo(poblacion):
+    #print("torneo")
+    retorno = []
+    retorno.append(poblacion[0] if poblacion[0].fitness > poblacion[9].fitness else poblacion[9])
+    retorno.append(poblacion[1] if poblacion[1].fitness > poblacion[8].fitness else poblacion[8])
+    retorno.append(poblacion[2] if poblacion[2].fitness > poblacion[7].fitness else poblacion[7])
+    retorno.append(poblacion[3] if poblacion[3].fitness > poblacion[6].fitness else poblacion[6])
+    retorno.append(poblacion[4] if poblacion[4].fitness > poblacion[5].fitness else poblacion[5])
+    return retorno
+
+def cruzarPadres(poblacion, train_set, val_set, capas1):
+    resultado = poblacion[:]
+    resultado.append(getHijo(poblacion[0], poblacion[1],train_set, val_set, capas1)) #1 con 2
+    resultado.append(getHijo(poblacion[2], poblacion[4],train_set, val_set, capas1)) #3 con 5
+    resultado.append(getHijo(poblacion[0], poblacion[2],train_set, val_set, capas1)) #1 con 3
+    resultado.append(getHijo(poblacion[2], poblacion[3],train_set, val_set, capas1)) #3 con 4
+    resultado.append(getHijo(poblacion[0], poblacion[4],train_set, val_set, capas1)) #1 con 5
+    random.shuffle(resultado)
+    return resultado
+
+def getHijo(padre1, padre2, train_set, val_set, capas1):
+    hijo = []
+    for i in range(4):
+        hijo.append(padre1.constantes[i] if porcentaje() else padre2.constantes[i])
+    hijo = mutarHijo(hijo)
+    return Individuo(hijo,entrenar_neuronas(train_set, val_set, capas1, hijo))
+
+def mutarHijo(hijo):
+    i = random.randint(0,3)
+    hijo[i] = random.randint(0,9)
+    return hijo
+
+#para obtener mis hyper paramaetros
+def get_hyperParams(alp, lamb, itera, keep):
+    tabla_alpa = [0.001, 0.075, 1, 0.000250, 0.099, 0.005, 0.000001, 0.2, 0.5, 0.0005725]
+    tabla_lamb = [0.0, 0.2, 0.05, 1.3, 0.75, 0.05, 0.1, 5, 7, 0.001]
+    tabla_iter = [2500, 1500, 750, 5000, 1000, 150, 1500, 8000, 9513, 4431]
+    tabla_keep = [1.0, 0.55, 0.12, 0.47, 0.96, 0.66, 0.88, 0.37, 0.78, 0.01]
+    return tabla_alpa[alp], tabla_lamb[lamb], tabla_iter[itera], tabla_keep[keep]
+
+#me regresa si muta o no
+def porcentaje(percent=50):
+    return random.randrange(100) < percent
+
+def programa():
+    #obtengo los valores 
+    train_X, train_Y, val_X, val_Y = Ar.lectura_dataset()
+
+    #creo los datasets
+    train_set = Data(train_X, train_Y)
+    val_set = Data(val_X, val_Y)
+
+    #definimos capas
+    capas1 = [train_set.n, 15, 7, 1]
+
+    poblacion = crear_poblacion(train_set, val_set, capas1)
+    loop = True
+    iteraciones = 1
+
+    while loop:
+        mejoresPadres = seleccionTorneo(poblacion)
+        poblacion = cruzarPadres(mejoresPadres, train_set, val_set, capas1)
+        if iteraciones == 3:
+            loop = False
+        iteraciones = iteraciones + 1
+    
+    mejor = sorted(poblacion, key=lambda x: x.fitness, reverse = True)
+    print("iteraciones: ", iteraciones)
+    return mejor[0]
+
+ganador = programa()
+alfa, lamb, ite, keep = get_hyperParams(ganador.constantes[0], ganador.constantes[1], ganador.constantes[2], ganador.constantes[3])
+print("Alpha: ", alfa)
+print("Lambda: ", lamb)
+print("Iteraciones: ", ite)
+print("Keep: ", keep)
+print("Fintess: ", ganador.fitness)
